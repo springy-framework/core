8203d9a656d5982913c2ed29391f766dea190c40
Fernando Val <fernando@fval.com.br>
2019-01-21 20:52:05 -0200

    Adding Handler Interface

---
+++ src/Events/HandlerInterface.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Event handler interface.
+ *
+ * @copyright 2014 Fernando Val
+ * @author    Fernando Val <fernando.val@gmail.com>
+ * @author    Allan Marques <allan.marques@ymail.com>
+ * @license   https://github.com/springy-framework/core/blob/master/LICENSE MIT
+ *
+ * @version   1.0.0
+ */
+
+namespace Springy\Events;
+
+/**
+ * Event handler interface.
+ */
+interface HandlerInterface
+{
+    /**
+     * Registers this class as handlers in the necessary events.
+     *
+     * @param Mediator $mediator
+     *
+     * @return void
+     */
+    public function subscribes(Mediator $mediator);
+}
