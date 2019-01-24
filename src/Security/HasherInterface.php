87da2d76ccf996954504529b1a1ed42f5712f68c
Fernando Val <fernando@fval.com.br>
2019-01-23 22:12:28 -0200

    Adding BCryptHasher class and Hasher interface

---
+++ src/Security/HasherInterface.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Hasher interface.
+ *
+ * @copyright 2014 Fernando Val
+ * @author    Allan Marques <allan.marques@ymail.com>
+ * @author    Fernando Val <fernando.val@gmail.com>
+ * @license   https://github.com/springy-framework/core/blob/master/LICENSE MIT
+ *
+ * @version   1.0.0
+ */
+
+namespace Springy\Security;
+
+/**
+ * Hasher interface.
+ */
+interface HasherInterface
+{
+    /**
+     * Creates and returns the generated hash of the entered string.
+     *
+     * @param string $stringToHash
+     * @param int    $times
+     *
+     * @return string
+     */
+    public function make(string $stringToHash, int $times): string;
+
+    /**
+     * Checks a password against a hash.
+     *
+     * @param string $stringToCheck
+     * @param string $hash
+     *
+     * @return bool
+     */
+    public function verify(string $stringToCheck, string $hash): bool;
+
+    /**
+     * Checks whether the string needs to be encrypted again.
+     *
+     * @param string $hash
+     * @param int    $times
+     *
+     * @return bool
+     */
+    public function needsRehash(string $hash, int $times): bool;
+}
---
+++ tests/Security/HasherTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Test case for Springy\Security\BCryptHasher class.
+ *
+ * @copyright 2014 Fernando Val
+ * @author    Allan Marques <allan.marques@ymail.com>
+ * @author    Fernando Val <fernando.val@gmail.com>
+ * @license   https://github.com/springy-framework/core/blob/master/LICENSE MIT
+ *
+ * @version   1.0.0
+ */
+use PHPUnit\Framework\TestCase;
+use Springy\Security\BCryptHasher as Hasher;
+
+/**
+ * Springy\Security\BCryptHasher
+ */
+class HasherTest extends TestCase
+{
+    public $hasher;
+
+    public function setUp()
+    {
+        $this->hasher = new Hasher();
+    }
+
+    public function testThatHasherCanGenerateASecureHash()
+    {
+        $hash = $this->hasher->make('password');
+
+        $this->assertGreaterThanOrEqual(60, strlen($hash));
+    }
+
+    public function testThatHasherCanVerifyTheHashedString()
+    {
+        $hash = $this->hasher->make('password');
+
+        $this->assertTrue($this->hasher->verify('password', $hash));
+    }
+
+    public function testThatHasherTellsIfAHashNeedsRehashing()
+    {
+        $hash = $this->hasher->make('password', 5);
+
+        $this->assertTrue($this->hasher->needsRehash($hash, 10));
+    }
+}
