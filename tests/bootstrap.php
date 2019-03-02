202c7820773221790815af5a15087099d524450c
Fernando Val <fernando@fval.com.br>
2019-03-02 16:30:37 -0300

    Creating var dir into test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,4 +15,8 @@ require __DIR__.'/../vendor/autoload.php';
 // Sets the default timezone
 date_default_timezone_set('UTC');
 
+if (!is_dir(__DIR__.'/var')) {
+    mkdir(__DIR__.'/var', 0775);
+}
+
 $app = new Springy\Core\Kernel(__DIR__.'/conf/main.php');
